ff9b3a78c3c14263736125b8aa6cee493e2de0aa
Giovane Souza <developergiovanesouza@gmail.com>
2024-11-21 00:25:48 -0300

    build: update Restsharp

--- FilteringSortingApiWinUi/FilteringSortingApiWinUi/MainWindow.xaml.cs
+++ FilteringSortingApiWinUi/FilteringSortingApiWinUi/MainWindow.xaml.cs
@@ -1,26 +1,12 @@
 using FilteringSortingApiWinUi.ControlPages;
-using FilteringSortingApiWinUi.controls;
 using FilteringSortingApiWinUi.Models;
-using FilteringSortingApiWinUi.Services;
 using FilteringSortingApiWinUi.Utils;
-using Microsoft.UI.Input;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Xml.Linq;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Popups;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
